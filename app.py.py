ff5703fdda8d1728c0e5356f73f51d918f4c5ca0
Kavita Pahadiya <99033559+kavitapahadiya70@users.noreply.github.com>
2022-08-05 21:07:17 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -26,6 +26,5 @@ def predict():
     
         
     return render_template('index.html', prediction_text='Regression Model  has predicted price for given sqft is : {}'.format(prediction))
-
-
+  if __name=='__main__':
 app.run(debug=True)
